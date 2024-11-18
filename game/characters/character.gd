c65c8cb16ee05a986ed146e41537a61a1717a596
Fio <leukert.fio@gmail.com>
2024-11-18 12:29:54 +0100

    upgrade more sprites and better speaking shader and new font

--- game/characters/character.gd
+++ game/characters/character.gd
@@ -10,6 +10,9 @@ signal repositioned()
 
 var emotions_by_page := {}
 
+@onready var active_mat = $Sprite.get_material()
+var light : PointLight2D
+
 func _ready():
 	ParserEvents.dialog_line_args_passed.connect(on_dialog_line_args_passed)
 	add_to_group("character")
@@ -28,23 +31,34 @@ func _ready():
 	#elif character_name == "one+anhedonia":
 		#set_x_position(4)
 	
+	#var l = PointLight2D.new()
+	#add_child(l)
+	#l.position = Vector2(126, 273)
+	#l.enabled = false
+	#l.texture = load("res://game/visuals/sigil_actuall.png")
+	#light = l
 
 func set_x_position(idx:int, time := 0, advance_instruction_after_reposition:=false):
-	var positions := [
-		-120,
-		0,
-		240,
-		307,
-		475,
-		960.0 / 2.0,
-		960 - 475,
-		960 - 307,
-		960 - 240,
-		960,
-		960 + 120
-	]
+	#var positions := [
+		#-120,
+		#0,
+		#240,
+		#307,
+		#475,
+		#960.0 / 2.0,
+		#960 - 475,
+		#960 - 307,
+		#960 - 240,
+		#960,
+		#960 + 120
+	#]
 	
-	target_x = positions[idx + 2]
+	var position0 = 240
+	var position6 = 960 - 240
+	var fraction = (position6 - position0) / 7
+	target_x = position0 + idx * fraction
+	#
+	#target_x = positions[idx + 2]
 	var t = create_tween()
 	t.tween_property(self, "position:x", target_x, time)
 	
@@ -58,6 +72,7 @@ func serialize() -> Dictionary:
 	result["emotion"] = emotion
 	result["target_x"] = target_x
 	result["emotions_by_page"] = emotions_by_page
+	#result["progress"] = active_mat.get_shader_parameter("progress")
 	
 	return result
 
@@ -66,16 +81,22 @@ func deserialize(data: Dictionary):
 	position.x = data.get("target_x", position.x)
 	visible = data.get("visible", false)
 	emotions_by_page = data.get("emotions_by_page", {})
+	#active_mat.set_shader_parameter("progress", data.get("progress", 0.0))
 
 func on_dialog_line_args_passed(actor_name: String, dialog_line_args: Dictionary):
 	var new_modulate:float
 	if actor_name == character_name:
-		new_modulate = 1.0
+		#active_mat.set_shader_parameter("BLUR_SIZE", 0.03)
+		#active_mat.set_shader_parameter("BLOOM_INTENSITY", 1)
+		#active_mat.set_shader_parameter("BLOOM_THRESHOLD", 0.8)
+		active_mat.set_shader_parameter("progress", 0.469)
+		#active_mat.set_shader_parameter("outline_c", Color.CORAL)
+		#light.enabled = true
 	else:
-		new_modulate = 0.8
+		active_mat.set_shader_parameter("progress", 0.0)
 	if emotion != "invisible" and actor_name == character_name:
 		visible = true
-	modulate.v = new_modulate
+	#modulate.v = new_modulate
 	if dialog_line_args.has(str(character_name, "-emotion")):
 		var new_emotion : String = dialog_line_args.get(str(character_name, "-emotion"))
 		emotion = new_emotion.trim_suffix("-emotion")
